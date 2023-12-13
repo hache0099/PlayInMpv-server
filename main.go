7fe44cbb36f26a89de3d7ffd854b063e7c8d61f2
hache0099 <hache0099_github@protonmail.com>
2023-12-12 21:59:18 -0300

    Added a function to handle playing urls

--- main.go
+++ main.go
@@ -3,13 +3,24 @@ package main
 import (
   "fmt"
   "net/http"
+  //"net/url"
+  "PlayInMPV/Exec"
 )
 
 func main(){
-  http.HandleFunc("/", func (w http.ResponseWriter, req *http.Request){
-    fmt.Fprintf(w, "Hello World")
+  http.HandleFunc("/play", func (w http.ResponseWriter, req *http.Request){
+    fmt.Fprintf(w, "<h1>Hello World</h1>")
+    options := req.URL.Query()
+
+    fmt.Println(options)
+
+    err := Exec.OpenLink(options["url"][0])
+    if err != nil{
+      panic(err)
+    }
   })
 
+  
   
   http.ListenAndServe(":8090",nil)
 }
